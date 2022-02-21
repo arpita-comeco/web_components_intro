42d4cfe17d964542d879ba386d2430c167ef73b5
arpita-comeco <arpita.behura@comeco.com>
2022-02-21 16:37:34 +0100

    Inserting shadow-root

--- userCard.js
+++ userCard.js
@@ -1,15 +1,22 @@
+const template = document.createElement('template');
+template.innerHTML= `
+<style>
+    h3 {
+        color: orange;
+    }
+</style>
+<div class="user-card">
+    <h3></h3>
+</div>
+`;
+
 class UserCard extends HTMLElement {
     constructor() {
         super();
-        this.attachShadow({ mode: 'open' })
-        this.innerHTML= `
-            <style>
-                h3 {
-                    color: orange;
-                }
-            </style>
-            <h3>${this.getAttribute('name')}</h3>
-        `;
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+        this.shadowRoot.querySelector('h3').innerText= this.getAttribute('name');
+
     }
 }
 
